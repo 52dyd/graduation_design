67486977ec533b8c6d9bd16c89a427f7f2bac185
yuwz <yuwz@hust.edu.cn>
2023-04-06 10:35:39 +0800

    A

---
+++ common/memory.py
@@ -0,0 +1,27 @@
+import random
+
+seed_value = 2020   # 设定随机数种子
+random.seed(seed_value)
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+    
+    def push(self, state_dict, action_dict, reward_dict, next_state_dict):
+        for key in state_dict.keys():
+            if key in next_state_dict.keys():
+                if len(self.buffer) < self.capacity:
+                    self.buffer.append(None)
+                #print('state:{}, action:{}, reward:{}, next_state:{}'.format(state_dict[key], action_dict[key], reward_dict[key][0], next_state_dict[key]))
+                self.buffer[self.position] = (state_dict[key], action_dict[key], reward_dict[key][0], next_state_dict[key])
+                self.position = (self.position + 1) % self.capacity
+    
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state =  zip(*batch)
+        return state, action, reward, next_state
+    
+    def __len__(self):
+        return len(self.buffer)
